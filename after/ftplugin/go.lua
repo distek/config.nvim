203e08845eff8a8a5385146de6b4db48b7f8be3e
distek <distek@protonmail.com>
2023-01-29 11:03:57 -0800

    Add go mappings and cleanup go after plugin

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -5,15 +5,9 @@ local map = function(mode, keys, command)
     vim.api.nvim_set_keymap(mode, keys, command, opts)
 end
 
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = { "*go" },
-    callback = function()
-        -- map("n", "<leader>Gfs", ":GoFillStruct<cr>")
-        -- map("n", "<leader>Gie", ":GoIfErr<cr>")
-        map("n", "<leader>Gat", ":GoAddTest<cr>")
-    end,
-    group = "Go",
-})
+map("n", "<leader>Gfs", ":GoFillStruct<cr>")
+map("n", "<leader>Gie", ":GoIfErr<cr>")
+map("n", "<leader>Gat", ":GoAddTest<cr>")
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = { "*go" },
@@ -26,55 +20,55 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- Add test for function under cursor
 -- Args:
 --     open: bool - opens test file after function creation
-function GoAddTest(open)
-    if vim.fn.executable("gotests") == 0 then
-        vim.notify("GoAddTest: gotests not in path or not installed", vim.log.levels.ERROR)
-        return
-    end
+-- function GoAddTest(open)
+--     if vim.fn.executable("gotests") == 0 then
+--         vim.notify("GoAddTest: gotests not in path or not installed", vim.log.levels.ERROR)
+--         return
+--     end
 
-    local bufNumber = vim.api.nvim_get_current_buf()
+--     local bufNumber = vim.api.nvim_get_current_buf()
 
-    local bufPath = vim.api.nvim_buf_get_name(bufNumber)
+--     local bufPath = vim.api.nvim_buf_get_name(bufNumber)
 
-    local ts_utils = require("nvim-treesitter.ts_utils")
+--     local ts_utils = require("nvim-treesitter.ts_utils")
 
-    local node = ts_utils.get_node_at_cursor()
-    if not node then
-        return
-    end
+--     local node = ts_utils.get_node_at_cursor()
+--     if not node then
+--         return
+--     end
 
-    local currentNode = node
+--     local currentNode = node
 
-    while currentNode do
-        if currentNode:type() == "function_declaration" then
-            break
-        end
-        currentNode = currentNode:parent()
-    end
+--     while currentNode do
+--         if currentNode:type() == "function_declaration" then
+--             break
+--         end
+--         currentNode = currentNode:parent()
+--     end
 
-    if not currentNode then
-        vim.notify("GoAddTest: Could not find parent", vim.log.levels.ERROR)
-        return
-    end
+--     if not currentNode then
+--         vim.notify("GoAddTest: Could not find parent", vim.log.levels.ERROR)
+--         return
+--     end
 
-    local funcName = vim.treesitter.query.get_node_text(currentNode:child(1), 0)
+--     local funcName = vim.treesitter.query.get_node_text(currentNode:child(1), 0)
 
-    if not funcName then
-        vim.notify("GoAddTest: No function under cursor", vim.log.levels.ERROR)
-        return
-    end
+--     if not funcName then
+--         vim.notify("GoAddTest: No function under cursor", vim.log.levels.ERROR)
+--         return
+--     end
 
-    local subRoot, _ = bufPath:gsub(vim.lsp.buf.list_workspace_folders()[1] .. "/", "")
+--     local subRoot, _ = bufPath:gsub(vim.lsp.buf.list_workspace_folders()[1] .. "/", "")
 
-    local pathSubGo = subRoot:gsub(".go$", "")
+--     local pathSubGo = subRoot:gsub(".go$", "")
 
-    os.execute(
-        'gotests -only "^' .. funcName .. '$" -w ' .. pathSubGo .. "_test.go " .. pathSubGo .. ".go "-- what I have to do
-    )
+--     os.execute(
+--         'gotests -only "^' .. funcName .. '$" -w ' .. pathSubGo .. "_test.go " .. pathSubGo .. ".go "-- what I have to do
+--     )
 
-    if open then
-        vim.cmd("edit " .. pathSubGo .. "_test.go")
-    end
-end
+--     if open then
+--         vim.cmd("edit " .. pathSubGo .. "_test.go")
+--     end
+-- end
 
-vim.api.nvim_create_user_command("GoAddTest", "lua GoAddTest(true)", {})
+-- vim.api.nvim_create_user_command("GoAddTest", "lua GoAddTest(true)", {})
