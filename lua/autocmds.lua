4c2843f235a7bd86dcff227113ba2465d4b51b6a
distek <distek@protonmail.com>
2023-02-09 06:06:17 -0800

    Add autocmd to keep terminal from changing bufs unexpectedly
    Like clicking on a buffer tab or something else weird happening

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -208,3 +208,29 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set("n", "q", ":close<cr>", { buffer = true, silent = true })
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = {
+		"*",
+	},
+	callback = function()
+		local tp = vim.api.nvim_get_current_tabpage()
+
+		if TF.Term[tp].window:is_valid() then
+			if vim.api.nvim_get_current_win() == TF.Term[tp].window.winid then
+				vim.defer_fn(function()
+					if #TF.Term[tp].bufs > 0 then
+						local nextBuf = TF.Term[tp].bufs[TF.Term[tp].last_term]
+						if not nextBuf then
+							nextBuf = TF.Term[tp].bufs[TF.Term[tp].last_term - 1]
+							if not nextBuf then
+								nextBuf = TF.Term[tp].bufs[TF.Term[tp].last_term + 1]
+							end
+						end
+						vim.api.nvim_win_set_buf(TF.Term[tp].window.winid, TF.Term[tp].bufs[TF.Term[tp].last_term])
+					end
+				end, 1)
+			end
+		end
+	end,
+})
