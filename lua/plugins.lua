29854d786f565ddf6a01bfbf7498c485b4376cbe
distek <distek@protonmail.com>
2023-02-20 18:42:32 -0800

    Formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -208,7 +208,6 @@ require("lazy").setup({
 					-- exactly match "impl_item" only)
 					-- rust = true,
 				},
-
 				-- [!] The options below are exposed but shouldn't require your attention,
 				--     you can safely ignore them.
 
@@ -659,9 +658,7 @@ require("lazy").setup({
 
 	-- Filetypes{{{
 	{ "chrisbra/csv.vim", ft = { "csv" }, lazy = true },
-
 	{ "rust-lang/rust.vim", ft = { "rust" }, lazy = true },
-
 	{ "sirtaj/vim-openscad", ft = { "openscad" }, lazy = true },
 
 	{
@@ -717,7 +714,7 @@ require("lazy").setup({
 			})
 
 			require("mason-lspconfig").setup({
-				ensure_installed = { "bashls", "clangd", "cssls", "gopls", "sumneko_lua", "tsserver" },
+				ensure_installed = { "bashls", "clangd", "cssls", "gopls", "lua_ls", "tsserver" },
 				automatic_installation = true,
 			})
 
@@ -823,7 +820,6 @@ require("lazy").setup({
 				log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
 				-- default is  ~/.cache/nvim/lsp_signature.log
 				verbose = false, -- show debug line number
-
 				bind = true, -- This is mandatory, otherwise border config won't get registered.
 				-- If you want to hook lspsaga or other signature handler, pls set to false
 				doc_lines = 0, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
@@ -835,9 +831,7 @@ require("lazy").setup({
 				max_width = 80, -- max_width of signature floating_window
 				noice = false, -- set to true if you using noice to render markdown
 				wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
-
 				floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
-
 				floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
 				-- will set to true when fully tested, set to false will use whichever side has more space
 				-- this setting will be helpful if you do not want the PUM and floating win overlap
@@ -855,21 +849,16 @@ require("lazy").setup({
 				handler_opts = {
 					border = "shadow", -- double, rounded, single, shadow, none, or a table of borders
 				},
-
 				always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-
 				auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
 				extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
 				zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-
 				padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
-
 				transparency = nil, -- disabled by default, allow floating win transparent value 1~100
 				shadow_blend = 36, -- if you using shadow as border use this set the opacity
 				shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
 				timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
 				toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-
 				select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
 				move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
 			})
@@ -877,7 +866,6 @@ require("lazy").setup({
 	}, -- }}}
 
 	{ "williamboman/mason.nvim", event = "BufReadPre" },
-
 	{ "williamboman/mason-lspconfig.nvim", event = "BufReadPre" },
 
 	{
@@ -1251,7 +1239,6 @@ require("lazy").setup({
 	{ "rcarriga/nvim-dap-ui", event = "VeryLazy" },
 
 	{ "theHamsta/nvim-dap-virtual-text", event = "VeryLazy" },
-
 	{ "mfussenegger/nvim-dap-python", ft = { "python" }, lazy = true },
 	-- }}}
 
@@ -1330,7 +1317,6 @@ require("lazy").setup({
 	},
 
 	{ "tpope/vim-fugitive", event = "VeryLazy", cmd = "Git" },
-
 	{ "ThePrimeagen/git-worktree.nvim", event = "VeryLazy" },
 
 	{ "powerman/vim-plugin-AnsiEsc" },
