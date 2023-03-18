b0b46af3be8588fa39c21959a555c319a7ab7dda
distek <distek@protonmail.com>
2023-03-18 09:50:46 -0700

    neovide stuff

--- lua/neovide-settings.lua
+++ lua/neovide-settings.lua
@@ -1,1 +1,21 @@
-vim.opt.guifont = "FiraCode Nerd Font Mono:h8"
+vim.opt.guifont = "FiraCode Nerd Font Mono:h12"
+
+local map = vim.keymap.set
+
+local function neovideScale(amount)
+	local temp = vim.g.neovide_scale_factor + amount
+
+	if temp < 0.5 then
+		return
+	end
+
+	vim.g.neovide_scale_factor = temp
+end
+
+map("n", "<C-=>", function()
+	neovideScale(0.1)
+end)
+
+map("n", "<C-->", function()
+	neovideScale(-0.1)
+end)
