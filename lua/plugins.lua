449b591bc4fc73c54826c31ee42c43ca05bd2edc
distek <distek@protonmail.com>
2023-04-09 14:31:52 -0700

    I don't think I need vim-markdown anymore?

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -620,19 +620,6 @@ require("lazy").setup({
 	{ "sirtaj/vim-openscad", ft = { "openscad" }, lazy = true },
 
 	{
-		"plasticboy/vim-markdown",
-		ft = { "markdown" },
-		lazy = true,
-		config = function()
-			vim.g.vim_markdown_folding_disabled = 1
-			vim.g.vim_markdown_no_default_key_mappings = 1
-			vim.g.vim_markdown_conceal = 1
-			vim.g.vim_markdown_conceal_code_blocks = 0
-			vim.g.vim_markdown_strikethrough = 1
-		end,
-	},
-
-	{
 		"ray-x/go.nvim",
 		ft = { "go" },
 		lazy = true,
