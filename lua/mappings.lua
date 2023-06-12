588935ffd73aca86db4a3c8f3fc9fa0cd3b909d7
distek <distek@protonmail.com>
2023-06-12 12:26:59 -0700

    Deprecation fix

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,9 +1,9 @@
 -- links
 local map = vim.keymap.set
 
-if vim.loop.os_uname().sysname == "Darwin" then
+if vim.uv.os_uname().sysname == "Darwin" then
 	map("n", "gx", 'yiW:!open <C-R>"<CR><Esc>')
-elseif vim.loop.os_uname().sysname == "Linux" then
+elseif vim.uv.os_uname().sysname == "Linux" then
 	map("n", "gx", 'yiW:!xdg-open <C-R>"<CR><Esc>')
 end
 
