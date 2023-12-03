4959dad94e99891e44d5b91bca5547fc5931d601
distek <distek@protonmail.com>
2023-12-03 07:51:44 -0800

    better openscad config

--- lua/plugins/filetype/openscad.lua
+++ lua/plugins/filetype/openscad.lua
@@ -1,1 +1,11 @@
-return { "sirtaj/vim-openscad", ft = { "openscad" }, lazy = true }
+return {
+	"sirtaj/vim-openscad",
+	ft = { "openscad" },
+	config = function()
+		Util.defer(function()
+			vim.cmd("set commentstring=//\\ %s")
+			vim.keymap.set("v", "<leader>cm", ":Commentary<cr>")
+		end, 50)
+	end,
+	lazy = true,
+}
